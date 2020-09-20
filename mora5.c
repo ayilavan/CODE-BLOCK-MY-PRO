bc5f6e3fa40cf345e4785c6581ebe06ed8fc6c18
lavan-code15 <71577824+lavan-code15@users.noreply.github.com>
2020-09-20 20:56:21 +0530

    Update mora5.c

--- mora5.c
+++ mora5.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
-int main()
+void main()
 {
     int x=(20||40)&& (60);//60 change any number result is "1" but change to 0, answer=0
     printf("x is %d",x);
-    return 0;
+    
 }
